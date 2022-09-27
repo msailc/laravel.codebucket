1a92795be71e4f255e90b89c9b78fbe80024e1ce
msailc <mustafaislamovich@gmail.com>
2022-09-27 19:46:30 +0200

    soft delete

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -11,7 +11,8 @@ class SkillController extends Controller
 {
     public function index()
     {
-        return SkillResource::collection(Skill::with('users', 'positions')->paginate());
+        return SkillResource::collection(Skill::with('users', 'positions')
+            ->orderBy('id','desc')->paginate());
     }
 
     public function show($id)
@@ -21,9 +22,15 @@ class SkillController extends Controller
 
     public function update(Request $request, $id)
     {
-        $team = Skill::findOrFail($id);
-        $team->update($request->only(['name']));
-        return response(new SkillResource($team), Response::HTTP_ACCEPTED);
+        $skill = Skill::findOrFail($id);
+        $skill->update($request->only(['name']));
+        return response(new SkillResource($skill), Response::HTTP_ACCEPTED);
+    }
+
+    public function store(Request $request)
+    {
+        $skill = Skill::create($request->only(['name']));
+        return response(new SkillResource($skill), Response::HTTP_CREATED);
     }
 
     public function destroy($id)
